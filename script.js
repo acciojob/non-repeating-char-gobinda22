6c7241f8c3877fe18a1af34325f3d089e9d99194
Gobinda Malakar <gobindamalakar68@gmail.com>
2023-05-05 15:56:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let s = prompt("Enter a string")
+var arr = [];
+for(let i=0;i<s.length;i++){
+    let ch = s[i];
+    let idx = ch.charCodeAt(0)
+    if(arr[idx - 'a'.charCodeAt(0)] >= 1){
+        let temp = arr[idx - 'a'.charCodeAt(0)];
+        arr[idx - 'a'.charCodeAt(0)] = temp + 1;
+    }
+    else{
+        arr[idx - 'a'.charCodeAt(0)] =  1;
+    }
+
+}
+for(let i=0;i<s.length;i++){
+     let ch = s[i];
+     let idx = ch.charCodeAt(0)
+     if(arr[idx - 'a'.charCodeAt(0)] == 1){
+         alert(ch);
+         break;
+     }
+}
